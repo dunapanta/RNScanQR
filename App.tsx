d5a1f218af453a939195ae02ba69e27d382b1cdb
Daniel Unapanta <dunapanta@outlook.es>
2023-01-25 23:20:44 -0500

    Add navigator appearence

--- App.tsx
+++ App.tsx
@@ -1,70 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import Lottie from 'lottie-react-native';
+import {WelcomeScreen} from './src/screens';
 
-import {MainButton} from './src/components';
-import {COLORS, SIZES, } from './src/constants';
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <View style={{flex: 1, padding: SIZES.padding}}>
-      <Text style={styles.sectionTitle}>Test</Text>
-
-      <View style={{flex: 1}}>
-        <Lottie
-          source={require('./src/assets/animations/qr-scan.json')}
-          autoPlay
-          loop
-        />
-      </View>
-      <MainButton
-        contentContainerStyle={{
-          height: 50,
-          borderRadius: SIZES.radius,
-        }}
-        label="Get Started"
-        onPress={() => console.log('test2')}
-      />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    color: 'red',
-    fontFamily: 'Poppins-Bold',
-  },
-  sectionTitle2: {
-    fontSize: 30,
-    fontFamily: 'Poppins-Italic',
-    color: 'blue',
-  },
-  sectionTitle3: {
-    fontSize: 80,
-    fontFamily: 'Poppins-Thin',
-    color: 'red',
-  },
-});
-
 export default App;
---
+++ src/screens/WelcomeScreen.tsx
@@ -0,0 +1,51 @@
+import {StyleSheet, Text, View} from 'react-native';
+import Lottie from 'lottie-react-native';
+
+import {MainButton} from '../components';
+import {COLORS, SIZES} from '../constants';
+
+export const WelcomeScreen = (): JSX.Element => {
+  return (
+    <View style={{flex: 1, padding: SIZES.padding}}>
+      <Text style={styles.sectionTitle}>Test</Text>
+
+      <View style={{flex: 1}}>
+        <Lottie
+          source={require('../assets/animations/qr-scan.json')}
+          autoPlay
+          loop
+        />
+      </View>
+      <MainButton
+        contentContainerStyle={{
+          height: 50,
+          borderRadius: SIZES.radius,
+        }}
+        label="Get Started"
+        onPress={() => console.log('test2')}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    color: 'red',
+    fontFamily: 'Poppins-Bold',
+  },
+  sectionTitle2: {
+    fontSize: 30,
+    fontFamily: 'Poppins-Italic',
+    color: 'blue',
+  },
+  sectionTitle3: {
+    fontSize: 80,
+    fontFamily: 'Poppins-Thin',
+    color: 'red',
+  },
+});
