0d972e5d11d2df8382960b98556f424c2b03eb4c
Daniel Unapanta <dunapanta@outlook.es>
2023-01-31 21:59:15 -0500

    Add animation with mot

--- src/components/onBoard/Animation2.tsx
+++ src/components/onBoard/Animation2.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, Image, View} from 'react-native';
 import {useDynamicAnimation, MotiImage} from 'moti';
 import {images, SIZES} from '../../constants';
@@ -29,9 +29,84 @@ export const Animation2 = ({animate}: Props) => {
     top: '27%',
     left: '50%',
   }));
+
+  useEffect(() => {
+    if (animate) {
+      motiImage1.animateTo({
+        top: '20%',
+        left: '15%',
+      });
+      motiImage2.animateTo({
+        top: '38%',
+        left: -10,
+      });
+      motiImage3.animateTo({
+        top: '62%',
+        left: '5%',
+      });
+      motiImage4.animateTo({
+        top: '75%',
+        left: '40%',
+      });
+      motiImage5.animateTo({
+        top: '15%',
+        left: '55%',
+      });
+    }
+  }, [animate]);
+
   return (
     <View style={{flex: 1, overflow: 'hidden'}}>
-      <Image source={images.walkthrough_02_01} />
+      <Image
+        source={images.walkthrough_02_01}
+        style={{
+          ...styles.image,
+          top: '35%',
+          left: '35%',
+          width: 106,
+          height: 161,
+          zIndex: 1,
+        }}
+      />
+
+      <Image
+        source={images.walkthrough_02_02}
+        style={{
+          ...styles.image,
+          top: '50%',
+          left: '50%',
+        }}
+      />
+
+      <MotiImage
+        state={motiImage1}
+        source={images.walkthrough_02_03}
+        style={styles.image}
+      />
+
+      <MotiImage
+        state={motiImage2}
+        source={images.walkthrough_02_04}
+        style={styles.image}
+      />
+
+      <MotiImage
+        state={motiImage3}
+        source={images.walkthrough_02_05}
+        style={styles.image}
+      />
+
+      <MotiImage
+        state={motiImage4}
+        source={images.walkthrough_02_06}
+        style={styles.image}
+      />
+
+      <MotiImage
+        state={motiImage5}
+        source={images.walkthrough_02_07}
+        style={styles.image}
+      />
     </View>
   );
 };
