99e3cda074ef878a0a66c0375178acb7b5cec345
Daniel Unapanta <dunapanta@outlook.es>
2023-02-05 23:10:47 -0500

    Add camera permissions and view

--- src/screens/ScanQRScreen.tsx
+++ src/screens/ScanQRScreen.tsx
@@ -1,18 +1,51 @@
-import {View} from 'react-native';
+import {useCallback, useEffect} from 'react';
+import {Linking, View} from 'react-native';
 import TextButton from '../components/shared/TextButton';
+import {Camera, useCameraDevices} from 'react-native-vision-camera';
 
 export const ScanQRScreen = ({navigation}: any) => {
+  //Camera
+  const devices = useCameraDevices();
+  const device = devices.back;
+
+  useEffect(() => {
+    requestCameraPermission();
+  }, []);
+
+  //Camera permission
+  const requestCameraPermission = useCallback(async () => {
+    const permission = await Camera.requestCameraPermission();
+    if (permission === 'denied') {
+      await Linking.openSettings();
+    }
+  }, []);
+
+  function renderCamera() {
+    if (!device || device === null) {
+      <View style={{flex: 1, backgroundColor: 'tomato'}}></View>;
+    } else {
+      return (
+        <Camera
+          style={{flex: 1, backgroundColor: 'purple'}}
+          device={device}
+          isActive={true}
+          enableZoomGesture={true}
+        />
+      );
+    }
+  }
+
   return (
     <View
       style={{
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
       }}>
-      <TextButton
+      {/* Camera */}
+      {renderCamera()}
+      {/* <TextButton
         label="Scan QR"
         onPress={() => navigation.navigate('QRCameraScreen')}
-      />
+      /> */}
     </View>
   );
 };
