19a0ed0a8218de2a54272cfd22a8fbcdb008e13c
Daniel Unapanta <dunapanta@outlook.es>
2023-02-09 22:53:40 -0500

    Add animation for result modal

--- src/components/QRScanner/ResultModal.tsx
+++ src/components/QRScanner/ResultModal.tsx
@@ -1,6 +1,6 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {Animated, Modal, TouchableWithoutFeedback, View} from 'react-native';
-import {COLORS} from '../../constants';
+import {COLORS, SIZES} from '../../constants';
 
 interface Props {
   isVisible: boolean;
@@ -10,12 +10,30 @@ interface Props {
 export const ResultModal = ({isVisible, onClose}: Props) => {
   const [showResultModal, setShowResultModal] = useState(false); //easy to animate the modal
 
+  const modalAnimatedValue = useRef(new Animated.Value(0)).current;
+
   useEffect(() => {
-    if (!showResultModal) {
-      onClose();
+    if (showResultModal) {
+      Animated.timing(modalAnimatedValue, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: false,
+      }).start();
+    } else {
+      Animated.timing(modalAnimatedValue, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: false,
+      }).start(() => onClose());
     }
     console.log('showResultModal: ', showResultModal);
   }, [showResultModal]);
+
+  const modalY = modalAnimatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [SIZES.height, SIZES.height - 6800],
+  });
+
   return (
     <Modal animationType="fade" transparent visible={isVisible}>
       <View style={{flex: 1, backgroundColor: 'red'}}>
@@ -29,10 +47,15 @@ export const ResultModal = ({isVisible, onClose}: Props) => {
         <Animated.View
           style={{
             position: 'absolute',
-            //top: 0,
+            top: modalY,
             left: 0,
-            right: 0,
-            bottom: 0,
+            width: '100%',
+            height: '100%',
+            padding: SIZES.padding,
+            borderTopRightRadius: SIZES.radius,
+            borderTopLeftRadius: SIZES.radius,
+            backgroundColor: COLORS.white,
+            zIndex: 100,
           }}></Animated.View>
       </View>
     </Modal>
